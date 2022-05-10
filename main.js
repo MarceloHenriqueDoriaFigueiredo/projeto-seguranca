c4e4c1021190eb9f6fa9991b738c8edb99ee2ea4
Marcos <marcosdev.gomes@gmail.com>
2022-05-10 11:34:58 -0300

    refatorando o codigo

---
+++ main.js
@@ -0,0 +1,20 @@
+var porcent = document.querySelector('.porcent');
+var paragrafo = document.querySelector('.paragrafo');
+var loading = document.querySelector('.loading');
+var count = 4;
+var load = setInterval(animate, 48);
+
+function animate(){
+    if(count == 100){
+        clearInterval(load);
+        paragrafo.innerHTML= 'SEUS DADOS FORAM ROUBADOS <br> COM SUCESSO'
+        porcent.innerHTML= '100% <br> Completo!'
+
+        
+
+    }
+    else{
+        ++count
+        porcent.textContent = count + '%';
+    }
+}
